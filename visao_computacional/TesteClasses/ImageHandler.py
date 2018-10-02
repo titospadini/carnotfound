3a5e3caa38be98680b1b67399702e873166048dc
Pedro Ivo <pi.cruz@outlook.com>
2018-10-02 13:36:41 -0300

    ImageHandler para piCamera e implementação de rotina de testes

--- visao_computacional/TesteClasses/ImageHandler.py
+++ visao_computacional/TesteClasses/ImageHandler.py
@@ -9,7 +9,7 @@
 # =============================================================
 #
 # DESCRICAO: CarHandler.py 
-# Python versao: 2
+# Python versao: 3
 #
 # Classe para manipular as imagens obtidas pela webcam com OpenCV.
 #
@@ -21,36 +21,58 @@ import time
 
 class ImageHandler():
 
-	#construtor da classe. O x0 sempre armazena a posição anterior da bolinha
-	def __init__(self):
-		self.x0 = 0
+	def tratarImagem(self, img):
+		LimiarBinarizacao = 125
+		AreaContornoLimiteMin = 5000
 
-	#a função de tratar o frame recebe a imagem "pura" da webcam e a transforma na matriz binária que indica onde está vermelho
-	def tratarFrame(self, frame):
-		frame = cv2.flip(frame, 1)
-		grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		(blueFrame, greenFrame, redFrame) = cv2.split(frame)
-		diffFrameRed = cv2.subtract(redFrame, grayFrame)
-		diffFrameRed = cv2.medianBlur(diffFrameRed, 15)
-		TRed, binRed = cv2.threshold(diffFrameRed, 50, 255, cv2.THRESH_BINARY)
-		return binRed
+		#obtencao das dimensoes da imagem
+		height = np.size(img, 0)
+		width= np.size(img, 1)
+		QtdeContornos = 0
+		DirecaoASerTomada = 0
+		print("Dimensões da imagem obtidas")
+		
+		#tratamento da imagem
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		gray = cv2.GaussianBlur(gray, (21, 21), 0)
+		FrameBinarizado = cv2.threshold(gray, LimiarBinarizacao, 255, cv2.THRESH_BINARY_INV)[1]
+		FrameBinarizado = cv2.dilate(FrameBinarizado, None, iterations=2)
+		FrameBinarizado = cv2.bitwise_not(FrameBinarizado)
+		#print("Tratamento 1 completo")
 
-	#função que abre uma janela no sistema operacional para mostrar o vídeo com a imagem já tratada
-	def exibirCaptura(self, binRed):
-		cv2.imshow('Resultado', binRed)
+		_, cnts, _ = cv2.findContours(FrameBinarizado.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		cv2.drawContours(img, cnts, -1, (255,0,255), 3)
+		#print("Tratamento 2 completo")
+		
+		for c in cnts:
+			#se a area do contorno capturado for pequena, nada acontece
+			if cv2.contourArea(c) < AreaContornoLimiteMin:
+				continue
+						
+			QtdeContornos = QtdeContornos + 1
+			#obtem coordenadas do contorno (na verdade, de um retangulo que consegue abrangir todo ocontorno) e
+			#realca o contorno com um retangulo.
+			(x, y, w, h) = cv2.boundingRect(c)   #x e y: coordenadas do vertice superior esquerdo #w e h: respectivamente largura e altura do retangulo
+			cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-	#função que verifica se o novo frame indica que a bolinha foi para a direita, esquerda, se está parada ou se não foi encontrada
-	def verificarSituacao(self, binRed):
-		M = cv2.moments(binRed)
-		if M['m00'] > 0:
-			x = int(M['m10'] / M['m00'])			
-			if x > self.x0 + 10:
-				situacao = "direita"
-			elif x < self.x0 - 10:
-				situacao = "esquerda"
-			else:
-				situacao = "parado"
-			self.x0 = x #a posição nova se torna a posição antiga
-		else:
-			situacao = "naoencontrado"
-		return situacao
+			#determina o ponto central do contorno e desenha um circulo para indicar
+			CoordenadaXCentroContorno = (x+x+w)//2
+			CoordenadaYCentroContorno = (y+y+h)//2
+			PontoCentralContorno = (CoordenadaXCentroContorno,CoordenadaYCentroContorno)
+			cv2.circle(img, PontoCentralContorno, 1, (0, 0, 0), 5)
+				
+			DirecaoASerTomada = CoordenadaXCentroContorno - (width//2)   #em relacao a linha central
+		
+		#print("Tratamento 3 completo")
+		#output da imagem
+		#linha em azul: linha central / referencia
+		#linha em verde: linha que mostra distancia entre linha e a referencia
+		cv2.line(img, (width//2, 0),(width//2, height),(255, 0, 0), 2)
+		
+		if (QtdeContornos > 0):
+			cv2.line(img, PontoCentralContorno, (width//2, CoordenadaYCentroContorno), (0, 255, 0), 1)
+		
+		#cv2.imshow('Analise de rota', img)
+		#cv2.waitKey(10)
+		
+		return DirecaoASerTomada, QtdeContornos
