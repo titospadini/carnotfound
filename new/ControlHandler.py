177f7cf1c9b9e45598eb172f95c9a7bb1399e3c2
Belle Orlandi <isabelle.dorlandi@hotmail.com>
2018-11-03 23:39:33 -0200

    .

--- new/ControlHandler.py
+++ new/ControlHandler.py
@@ -32,9 +32,9 @@ class  ControlHandler():
 		return angle
 			
 	def Linearizacao(self,x,y):
-		#V = np.array([x**1, x**0]).transpose()
-		#coef = ((np.linalg.inv((V.transpose()).dot(V))).dot(V.transpose())).dot(y)
-		coef = np.polyfit(x,y,1,full=False)		
+		V = np.array([x**1, x**0]).transpose()
+		coef = ((np.linalg.inv((V.transpose()).dot(V))).dot(V.transpose())).dot(y)
+		#coef = np.polyfit(x,y,1,full=False)		
 		return coef
 			
 			
